328a73cc544c022f35a4e93ada1538bd9cffef33
ars1992 <alessandro.salomon@gmx.de>
2023-04-09 22:16:17 +0200

    Main implementiert: noch recht lang. Steuer von leibeigener evt nicht richtig da auch bei < 12 1 zurückgegeben wird.

--- src/Königreich.java
+++ src/Königreich.java
@@ -3,14 +3,50 @@ import java.util.Scanner;
 public class Königreich {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
+        Einwohner könig, adel, bauer, leibeigen;
         String beenden;
         do{
+            System.out.println("Wählen Sie die Bevölkerungsgruppe");
+            System.out.println("1 - König");
+            System.out.println("2 - Adel");
+            System.out.println("3 - Bauer");
+            System.out.println("4 - Leibeigen");
+            int bevölgerungsgruppe = eingabe.nextInt();
 
+            System.out.println("Geben Sie den Namen an: ");
+            String name = eingabe.next();
 
+            System.out.println("Geben Sie das Einkommen an: ");
+            int einkommen = eingabe.nextInt();
+
+            switch (bevölgerungsgruppe) {
+                case 1 -> {
+                    könig = new König(name, einkommen);
+                    System.out.println(könig.steuer());
+                }
+                case 2 -> {
+                    adel = new Adel(name, einkommen);
+                    System.out.println(adel.steuer());
+                }
+                case 3 -> {
+                    bauer = new Bauer(name, einkommen);
+                    System.out.println(bauer.steuer());
+                }
+                case 4 -> {
+                    leibeigen = new Leibeigen(name, einkommen);
+                    System.out.println(leibeigen.steuer());
+                }
+                default -> {
+                    System.out.println("""
+                            Abfrage nicht möglich.
+                            Ungültige Eingabe.
+                            """);
+                }
+            }
 
             System.out.println("Möchten Sie das Programm beenden? (j/n)");
-            beenden = eingabe.nextLine();
+            beenden = eingabe.next();
 
-        }while (beenden.equalsIgnoreCase("j"));
+        }while ( ! beenden.equalsIgnoreCase("j"));
     }
 }
