dc146ed711288a83018c45fecd042e66718b77ee
ars1992 <alessandro.salomon@gmx.de>
2023-04-11 11:24:12 +0200

    main in methoden aufgeteilt Problem bei hinzukommen einer gruppe drei änderungen nötig

--- src/Königreich.java
+++ src/Königreich.java
@@ -3,15 +3,9 @@ import java.util.Scanner;
 public class Königreich {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
-        Einwohner könig, adel, bauer, leibeigen;
-        String beenden;
         do{
-            System.out.println("Wählen Sie die Bevölkerungsgruppe");
-            System.out.println("1 - König");
-            System.out.println("2 - Adel");
-            System.out.println("3 - Bauer");
-            System.out.println("4 - Leibeigen");
-            int bevölgerungsgruppe = eingabe.nextInt();
+            int bevölgerungsgruppe = bevölgerungsgruppe();
+            if ( ! eingabeGültig(bevölgerungsgruppe)) continue;
 
             System.out.println("Geben Sie den Namen an: ");
             String name = eingabe.next();
@@ -19,34 +13,58 @@ public class Königreich {
             System.out.println("Geben Sie das Einkommen an: ");
             int einkommen = eingabe.nextInt();
 
-            switch (bevölgerungsgruppe) {
-                case 1 -> {
-                    könig = new König(name, einkommen);
-                    System.out.println(könig.steuer());
-                }
-                case 2 -> {
-                    adel = new Adel(name, einkommen);
-                    System.out.println(adel.steuer());
-                }
-                case 3 -> {
-                    bauer = new Bauer(name, einkommen);
-                    System.out.println(bauer.steuer());
-                }
-                case 4 -> {
-                    leibeigen = new Leibeigen(name, einkommen);
-                    System.out.println(leibeigen.steuer());
-                }
-                default -> {
-                    System.out.println("""
+            ausgabeSteuern(bevölgerungsgruppe, name, einkommen);
+
+        }while ( ! beenden().equalsIgnoreCase("j"));
+    }
+
+    public static int bevölgerungsgruppe(){
+        Scanner eingabe = new Scanner(System.in);
+        System.out.println("Wählen Sie die Bevölkerungsgruppe");
+        System.out.println("1 - König");
+        System.out.println("2 - Adel");
+        System.out.println("3 - Bauer");
+        System.out.println("4 - Leibeigen");
+        return eingabe.nextInt();
+    }
+
+    public static String beenden(){
+        Scanner eingabe = new Scanner(System.in);
+        System.out.println("Möchten Sie das Programm beenden? (j/n)");
+        return eingabe.next();
+    }
+
+    public static boolean eingabeGültig(int bevölgerungsgruppe){
+        int anzahlBevölkerungsGruppen = 4;
+        if (bevölgerungsgruppe < 0 || bevölgerungsgruppe > anzahlBevölkerungsGruppen){
+            System.out.println("""
                             Abfrage nicht möglich.
                             Ungültige Eingabe.
                             """);
-                }
+            return false;
+        }
+        return true;
+    }
+
+    public static void ausgabeSteuern(int bevölgerungsgruppe, String name, int einkommen){
+        Einwohner könig, adel, bauer, leibeigen;
+        switch (bevölgerungsgruppe) {
+            case 1 -> {
+                könig = new König(name, einkommen);
+                System.out.println(könig.steuer());
             }
-
-            System.out.println("Möchten Sie das Programm beenden? (j/n)");
-            beenden = eingabe.next();
-
-        }while ( ! beenden.equalsIgnoreCase("j"));
+            case 2 -> {
+                adel = new Adel(name, einkommen);
+                System.out.println(adel.steuer());
+            }
+            case 3 -> {
+                bauer = new Bauer(name, einkommen);
+                System.out.println(bauer.steuer());
+            }
+            case 4 -> {
+                leibeigen = new Leibeigen(name, einkommen);
+                System.out.println(leibeigen.steuer());
+            }
+        }
     }
 }
